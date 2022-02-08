69e82f8e221f3cc2d1f56f8f7d393ab1f8135f93
dajo04002 <92377225+dajo04002@users.noreply.github.com>
2022-02-08 15:56:19 +0100

    Add files via upload

---
+++ Insertionsort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Insertionsort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j, key, i;
+            
+            int[] arr = {5,2,1,8,3,9,7,4,6};
+
+
+            for (i = 0; i < arr.Length; i++)
+            {
+                key = arr[i];
+                j = i - 1;
+                
+                while(j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+
+            foreach(int e in arr)
+            {
+                Console.WriteLine(e);
+            }
+            
+        }
+    }
+}

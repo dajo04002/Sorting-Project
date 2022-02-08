4bd29687b06b5c4cb6543c2a92f980b463a7faa2
dajo04002 <92377225+dajo04002@users.noreply.github.com>
2022-02-08 15:08:49 +0100

    Add files via upload

---
+++ Bubblesort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Intersetionsort
+{
+    class Program
+    {
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = {5,2,1,8,3,9,7,4,6};
+
+            for (int j = 0; j < arr.Length; j++)
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    if(arr[i] > arr[i+1])
+                    {
+                        Swap(ref arr[i], ref arr[i+1]);
+                    }
+                }
+            }
+            foreach(int e in arr)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+    }
+}

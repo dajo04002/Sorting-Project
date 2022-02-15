28aeef91fe0fb7a15c0f56b2f1f3ed98c36f2575
dajo04002 <92377225+dajo04002@users.noreply.github.com>
2022-02-15 15:22:26 +0100

    Add files via upload

---
+++ scrambler/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace scrambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 1000;
+            int[] arr = new int[length];
+            
+            for (int i = 0; i <= length - 1; i++)
+            {
+                arr[i] = i + 1;
+            }
+
+            Random random = new Random();
+            arr = arr.OrderBy(x => random.Next()).ToArray();
+            foreach (var i in arr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
